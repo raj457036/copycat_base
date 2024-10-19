373ad1ffa9150509a40e5f3f706f1b29384ee7f6
Raj <rs457036@gmail.com>
2024-10-19 14:49:23 +0530

    _

--- lib/l10n/generated/app_localizations_de.dart
+++ lib/l10n/generated/app_localizations_de.dart
@@ -2,8 +2,6 @@ import 'package:intl/intl.dart' as intl;
 
 import 'app_localizations.dart';
 
-// ignore_for_file: type=lint
-
 /// The translations for German (`de`).
 class AppLocalizationsDe extends AppLocalizations {
   AppLocalizationsDe([String locale = 'de']) : super(locale);
