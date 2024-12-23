49d858f4eed927cb0362ebed8329174207e345db
Raj <rs457036@gmail.com>
2024-12-23 14:06:40 +0530

    feat: link preview

--- lib/widgets/link_preview.dart
+++ lib/widgets/link_preview.dart
@@ -1,9 +1,16 @@
 import 'package:any_link_preview/any_link_preview.dart';
-import 'package:flutter/widgets.dart';
+import 'package:copycat_base/constants/widget_styles.dart';
+import 'package:copycat_base/utils/common_extension.dart';
+import 'package:flutter/material.dart';
 
 class LinkPreview extends StatelessWidget {
   final String url;
-  const LinkPreview({super.key, required this.url});
+  final bool expanded;
+  const LinkPreview({
+    super.key,
+    required this.url,
+    this.expanded = false,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -11,23 +18,86 @@ class LinkPreview extends StatelessWidget {
     if (!isValidUrl) {
       return SizedBox.shrink();
     }
+    final placeholder = Card.filled(
+      child: Center(child: CircularProgressIndicator()),
+    );
     return AnyLinkPreview.builder(
-      link: url,
-      itemBuilder: (context, meta, provider, svg) {
-        return Row(
-          children: [
-            if (provider != null) Image(image: provider),
-            Expanded(
+        link: url,
+        placeholderWidget:
+            expanded ? Expanded(child: placeholder) : placeholder,
+        errorWidget: SizedBox.shrink(),
+        cache: Duration(days: 30),
+        itemBuilder: (context, meta, provider, svg) {
+          if (meta.title == null && meta.desc == null && provider == null) {
+            return const SizedBox.shrink();
+          }
+          final colors = context.colors;
+          final content = Card.filled(
+            margin: EdgeInsets.zero,
+            shape: RoundedRectangleBorder(
+              borderRadius: radius8,
+            ),
+            child: ClipRRect(
+              borderRadius: radius8,
               child: Column(
+                spacing: 8,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  if (meta.title != null) Text(meta.title!),
-                  if (meta.desc != null) Text(meta.desc!),
+                  if (provider != null)
+                    Expanded(
+                      child: Image(
+                          image: provider,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) =>
+                              const SizedBox.shrink()),
+                    )
+                  else if (svg != null)
+                    Expanded(child: svg),
+                  if (meta.title != null || meta.desc != null)
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(padding4),
+                        child: Column(
+                          spacing: 4,
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            if (meta.title != null && meta.title!.isNotEmpty)
+                              Flexible(
+                                child: Text(
+                                  meta.title!,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                    // color: colors.primary,
+                                  ),
+                                ),
+                              ),
+                            if (meta.desc != null && meta.desc!.isNotEmpty)
+                              Flexible(
+                                child: Text(
+                                  meta.desc!,
+                                  overflow: TextOverflow.fade,
+                                  maxLines: 4,
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    color: colors.outline,
+                                  ),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                    )
                 ],
               ),
             ),
-          ],
-        );
-      },
-    );
+          );
+          if (expanded) return Expanded(child: content);
+          return content;
+        });
   }
 }
