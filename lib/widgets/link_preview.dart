86b04ec39d13b856d72017866f3b2b840a584bf3
Raj <rs457036@gmail.com>
2024-12-31 17:17:58 +0530

    refactor: ui tweaks

--- lib/widgets/link_preview.dart
+++ lib/widgets/link_preview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:any_link_preview/any_link_preview.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:copycat_base/constants/widget_styles.dart';
@@ -7,10 +8,21 @@ import 'package:flutter/material.dart';
 class LinkPreview extends StatelessWidget {
   final String url;
   final bool expanded;
+  final bool hideDesc;
+  final bool hideTitle;
+  final int maxTitleLines;
+  final int maxDescLines;
+  final bool withProgress;
+
   const LinkPreview({
     super.key,
     required this.url,
     this.expanded = false,
+    this.hideDesc = false,
+    this.hideTitle = false,
+    this.maxTitleLines = 2,
+    this.maxDescLines = 4,
+    this.withProgress = false,
   });
 
   @override
@@ -26,12 +38,15 @@ class LinkPreview extends StatelessWidget {
         errorWidget: const SizedBox.shrink(),
         cache: Duration(days: 30),
         itemBuilder: (context, meta, provider, svg) {
+          if (withProgress && !meta.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
           if (meta.title == null && meta.desc == null && provider == null) {
             return const SizedBox.shrink();
           }
           final colors = context.colors;
           provider as NetworkImage?;
-          final content = Card(
+          Widget content = Card(
             margin: EdgeInsets.zero,
             shape: RoundedRectangleBorder(
               borderRadius: radius8,
@@ -39,8 +54,8 @@ class LinkPreview extends StatelessWidget {
             child: ClipRRect(
               borderRadius: radius8,
               child: Column(
-                spacing: 8,
-                mainAxisSize: MainAxisSize.min,
+                spacing: 4,
+                mainAxisSize: MainAxisSize.max,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   if (provider != null)
@@ -55,48 +70,61 @@ class LinkPreview extends StatelessWidget {
                     )
                   else if (svg != null)
                     Expanded(child: svg),
-                  if (meta.title != null || meta.desc != null)
-                    Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.all(padding4),
-                        child: Column(
-                          spacing: 4,
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            if (meta.title != null && meta.title!.isNotEmpty)
-                              Flexible(
-                                child: Text(
-                                  meta.title!,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 2,
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    // color: colors.primary,
-                                  ),
+                  if ((meta.title != null || meta.desc != null) &&
+                      (!hideDesc || !hideTitle))
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: padding6,
+                        right: padding6,
+                        bottom: padding6,
+                      ),
+                      child: Column(
+                        spacing: 4,
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (meta.title != null &&
+                              meta.title!.isNotEmpty &&
+                              !hideTitle)
+                            Flexible(
+                              child: Text(
+                                meta.title!,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: maxTitleLines,
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600,
+                                  // color: colors.primary,
                                 ),
                               ),
-                            if (meta.desc != null && meta.desc!.isNotEmpty)
-                              Flexible(
-                                child: Text(
-                                  meta.desc!,
-                                  overflow: TextOverflow.fade,
-                                  maxLines: 4,
-                                  style: TextStyle(
-                                    fontSize: 10,
-                                    color: colors.outline,
-                                  ),
+                            ),
+                          if (meta.desc != null &&
+                              meta.desc!.isNotEmpty &&
+                              !hideDesc)
+                            Flexible(
+                              child: Text(
+                                meta.desc!,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: maxDescLines,
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: colors.outline,
                                 ),
                               ),
-                          ],
-                        ),
+                            ),
+                        ],
                       ),
                     )
                 ],
               ),
             ),
           );
+          if (withProgress) {
+            content = FadeIn(
+              delay: const Duration(milliseconds: 150),
+              child: content,
+            );
+          }
           if (expanded) return Expanded(child: content);
           return content;
         });
